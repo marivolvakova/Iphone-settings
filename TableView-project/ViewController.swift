78f5ae8c6efd605967b6738310be4299fb1d8ca1
marivolvakova <104025325+marivolvakova@users.noreply.github.com>
2022-06-15 17:58:21 +0300

    Initial Commit

---
+++ TableView-project/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  TableView-project
+//
+//  Created by Мария Вольвакова on 15.06.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
