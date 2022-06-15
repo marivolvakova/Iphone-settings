16248e2b2f5245e61b329aa6f45787ac248694d5
marivolvakova <104025325+marivolvakova@users.noreply.github.com>
2022-06-15 18:20:43 +0300

    App folder: SceneDelegate changes.

--- TableView-project/Sourses/App/SceneDelegate.swift
+++ TableView-project/Sourses/App/SceneDelegate.swift
@@ -14,14 +14,14 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
 
     func scene(_ scene: UIScene, willConnectTo session: UISceneSession, options connectionOptions: UIScene.ConnectionOptions) {
         guard let windowScene = (scene as? UIWindowScene) else { return }
-        window = UIWindow(windowScene: windowScene)
-        
-        let viewController = ViewController()
-        let navigationController = UINavigationController(rootViewController: viewController)
-        
-        
-        window?.rootViewController = navigationController
-        window?.makeKeyAndVisible()
+               window = UIWindow(windowScene: windowScene)
+               
+               let viewController = IphoneSettingsController()
+               let navigationController = UINavigationController(rootViewController: viewController)
+               
+               
+               window?.rootViewController = navigationController
+               window?.makeKeyAndVisible()
     }
 
 
